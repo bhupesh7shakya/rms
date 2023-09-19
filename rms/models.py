619d5430393360739f617a35a4d138a5a1e26d0a
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-09-19 18:07:56 +0545

    added:model created for django

--- rms/models.py
+++ rms/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User=get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+class Food(models.Model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=5,decimal_places=2)
+    category=models.ForeignKey(Category,on_delete=models.PROTECT)
+    
+class Table(models.Model):
+    number=models.IntegerField()
+    is_occupied=models.BooleanField(default=False)
+    
+class Profile(models.Model):
+    MALE_CHOICE='M'
+    FEMALE_CHOICE='F'
+    OTHER_CHOICE='O'
+    
+    GENDER_CHOICES=[
+        (MALE_CHOICE,'MALE'),
+        (FEMALE_CHOICE,'FEMALE'),
+        (OTHER_CHOICE,'FEMALE'),
+    ]
+    phone_number=models.CharField(max_length=10)
+    gender=models.CharField(max_length=1,choices=GENDER_CHOICES)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    
+    
+    
+class Order(models.Model):
+    PENDING_STATUS='P'
+    COOKING_STATUS='CO'
+    CANCELLED_STATUS='CA'
+    COMPLETED_STATUS='C'
+    
+    ORDER_STATUS=[
+        (PENDING_STATUS,'PENDING'),
+        (COOKING_STATUS,'COOKING'),
+        (CANCELLED_STATUS,'CANCELLED'),
+        (COMPLETED_STATUS,'COMPLETED'),
+        
+    ]
+    
+    
+    table=models.ForeignKey(Table,on_delete=models.PROTECT)
+    user=models.ForeignKey(User,on_delete=models.PROTECT)
+    payment_status=models.BooleanField(default=False)
+    status=models.CharField(choices=ORDER_STATUS,max_length=2,default=PENDING_STATUS)
