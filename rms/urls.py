d66cc0d896375e1624f58c60eef7f768e42b4bbb
bhupesh shakya <bhupesh7shakya@gmail.com>
2023-09-28 15:58:50 +0545

    simple crud api of category

--- rms/urls.py
+++ rms/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-
+from . import views
 urlpatterns = [
-    
+    path('category',views.category_list),
+    path('category/<pk>',views.category_detail),
 ]
--- rms/views.py
+++ rms/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+
+from .models import *
+from .serializers import *
+
+
+class CategoryView(APIView):
+    def get(self,*args,**kwargs):
+        categories = Category.objects.all()
+        serializer=CategorySerializer(categories,many=True)
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
 # Create your views here.
+@api_view(['GET','POST'])
+def category_list(request):
+    if request.method == "GET":
+        categories = Category.objects.all()
+        serializer=CategorySerializer(categories,many=True)
+        
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    else:
+        serializer=CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            
+        return Response(
+            serializer.data
+            ,status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET',"PATCH","DELETE"])        
+def category_detail(request,pk):
+    category=get_object_or_404(Category,pk=pk)
+    if request.method=="GET":
+        serializer=CategorySerializer(category)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    if request.method=="PATCH":
+        
+        serializer=CategorySerializer(category,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+            serializer.data
+            ,status=status.HTTP_200_OK
+            )
+            
+    if request.method=="DELETE":
+        food=Food.objects.filter(category=category)
+        if food.count()>0:
+            return Response({
+                "detail":"You can't delete data Its is protect "
+            })
+        category.delete()
+        return Response(
+            {"detail":"Data has been deleted"}
+            ,status=status.HTTP_204_NO_CONTENT
+            )
+    
+
+
+
